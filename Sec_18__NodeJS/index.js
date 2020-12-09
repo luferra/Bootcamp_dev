09a0ef321da7861b39d8a95f4da572090a88969f
luferra <ferrari.luca@live.it>
2020-12-09 11:29:00 +0100

    Modulo NodeJS

---
+++ Sec_18__NodeJS/index.js
@@ -0,0 +1,7 @@
+const superheroes = require('superheroes');
+var supervillains = require('supervillains');
+
+var mySH = superheroes.random();
+var mySV = supervillains.random();
+console.log(mySH);
+console.log(mySV);
