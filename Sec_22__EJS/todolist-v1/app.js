9205a7701626eab908fd248cbc235a6e2d504524
luferra <ferrari.luca@live.it>
2021-01-06 14:04:40 +0100

    end EJS + blog

--- Sec_22__EJS/todolist-v1/app.js
+++ Sec_22__EJS/todolist-v1/app.js
@@ -1,40 +1,50 @@
+//jshint esversion:6
+
 const express = require("express");
 //const https = require("https");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
-const PORT = 3000;
-//usa sempre let al posto di var. le variabili let, come i const sono locali anche all'interno di loop e if. Le variabili var invece sono globali se inserite in loop o if.
-let items = ["Buy food", "Cook food", "Eat food"];
+//è consigliabile utilizzare const per varibili che non cambiano. Ma gli array possono essere dichiarati come costanti.
+const items = ["Buy food", "Cook food", "Eat food"];
+const workItem = [];
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
+  //usa sempre let al posto di var. le variabili let, come i const sono locali anche all'interno di loop e if. Le variabili var invece sono globali se inserite in loop o if.
 
-    //https://stackoverflow.com/questions/3552461/how-to-format-a-javascript-date
-    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
-    let today = new Date();
-    // let weekday = new Array(7);
-    // weekday[0] = "Sunday";
-    // weekday[1] = "Monday";
-    // weekday[2] = "Tuesday";
-    // weekday[3] = "Wednesday";
-    // weekday[4] = "Thursday";
-    // weekday[5] = "Friday";
-    // weekday[6] = "Saturday";
-    // let day = weekday[today.getDay()];
-    let day = today.toLocaleDateString("it-IT", options);
-    res.render('list', { dayz: day, newIteme: items });
+  let day = date.getDayDate();
+  res.render('list', {
+    listTitle: day,
+    newIteme: items
+  });
 })
 
 app.post("/", function(req, res) {
-    let item = req.body.newItem;
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItem.push(item);
+    res.redirect("/work");
+  } else {
     items.push(item);
     res.redirect("/");
+  }
 })
 
-app.listen(PORT, function() {
-    console.log("listen server " + PORT);
+app.get("/work", function(req, res) {
+  res.render('list', {
+    listTitle: "Work",
+    newIteme: workItem
+  })
+})
+
+app.listen(3000, function() {
+  console.log("listen server 3000");
 })
